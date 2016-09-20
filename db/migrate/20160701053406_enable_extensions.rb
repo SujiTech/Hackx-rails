f47b353a16d37917f51068d2f5767f7e48970487
Chunlea Ju <ichunlea@me.com>
2016-09-21 00:47:28 +0800

    Enable extensions for postgresql

---
+++ db/migrate/20160701053406_enable_extensions.rb
@@ -0,0 +1,6 @@
+class EnableExtensions < ActiveRecord::Migration[5.0]
+  def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    enable_extension 'hstore' unless extension_enabled?('hstore')
+  end
+end
