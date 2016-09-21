465d0167f9c5ea44c7ca0d3b874043e58bb68007
Chunlea Ju <ichunlea@me.com>
2016-09-21 17:44:49 +0800

    Disable create extension for Postgresql

--- db/migrate/20160701053406_enable_extensions.rb
+++ db/migrate/20160701053406_enable_extensions.rb
@@ -1,6 +1,7 @@
 class EnableExtensions < ActiveRecord::Migration[5.0]
   def change
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
-    enable_extension 'hstore' unless extension_enabled?('hstore')
+    # Disabled for now
+    # enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    # enable_extension 'hstore' unless extension_enabled?('hstore')
   end
 end
